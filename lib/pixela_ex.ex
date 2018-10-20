2f4f4b6578737f55c0c19b47d245cd7c9528fc78
otoyo <hiroki.t.1988@gmail.com>
2018-10-20 12:34:16 +0900

    Add typespec

--- lib/pixela_ex.ex
+++ lib/pixela_ex.ex
@@ -1,18 +1,48 @@
 defmodule PixelaEx do
   @moduledoc """
-  Documentation for PixelaEx.
+  API Client for Pixela.
   """
 
-  @doc """
-  Hello world.
+  use PixelaEx.Client.UserFunctions
+  use PixelaEx.Client.GraphFunctions
+  use PixelaEx.Client.PixelFunctions
 
-  ## Examples
+  @typedoc "User name for Pixela"
+  @type username :: String.t
 
-      iex> PixelaEx.hello
-      :world
+  @typedoc "A token string used to authenticate as a user to be created"
+  @type token :: String.t
 
-  """
-  def hello do
-    :world
-  end
+  @typedoc "Whether you agree to the terms of service"
+  @type agree_terms_of_service :: boolean()
+
+  @typedoc "Whether you are not a minor or if you are a minor and you have the parental consent of using Pixela"
+  @type not_minor :: boolean()
+
+  @typedoc "A new authentication token"
+  @type new_token :: String.t
+
+  @typedoc "An ID for identifying the pixelation graph"
+  @type graph_id :: String.t
+
+  @typedoc "the name of the pixelation graph"
+  @type name :: String.t
+
+  @typedoc "A unit of the quantity recorded in the pixelation graph. Ex. commit, kilogram, calory."
+  @type unit :: String.t
+
+  @typedoc "The type of quantity to be handled in the graph. Only `int` or `float` are supported."
+  @type type :: String.t
+
+  @typedoc "The display color of the pixel in the pixelation graph. `shibafu`, `momiji`, `sora`, `ichou`, `ajisai` and `kuro` are supported as color kind."
+  @type color :: String.t
+
+  @typedoc "The date on which the quantity is to be recorded. It is specified in yyyyMMdd format."
+  @type date :: String.t
+
+  @typedoc "The quantity to be registered on the specified date."
+  @type quantity :: String.t
+
+  @typedoc "HTTP response from HTTPotion"
+  @type http_result :: HTTPotion.Response.t | %HTTPotion.AsyncResponse{} | %HTTPotion.ErrorResponse{}
 end
--- test/pixela_ex_test.exs
+++ test/pixela_ex_test.exs
@@ -1,8 +1,4 @@
 defmodule PixelaExTest do
   use ExUnit.Case
-  doctest PixelaEx
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
